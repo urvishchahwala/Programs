b8203f23492d5639cc1b9b8a07554447d1c52366
Urvish Chahwala <35097100+urvishchahwala@users.noreply.github.com>
2020-08-20 00:39:07 -0500

    Update Rectangle.java

--- Task6/Rectangle.java
+++ Task6/Rectangle.java
@@ -1,5 +1,14 @@
 /**
-* 
+*  Create a class named 'Rectangle' with two data members- length and breadth and a method to calculate the area which is 'length*breadth'. The class has three constructors which are : 
+
+*   1) having no parameter - values of both length and breadth are assigned zero. 
+
+*   2) having two numbers as parameters - the two numbers are assigned as length and breadth respectively. 
+
+*   3) having one number as parameter - both length and breadth are assigned that number. 
+
+*  Now, create objects of the 'Rectangle' class having none, one and two parameters and print their areas. 
+ 
 */
 public class Rectangle {
 
