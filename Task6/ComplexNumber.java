7d8ab9b36a6323d15855e79d0a765a9d109a14ad
Urvish Chahwala <35097100+urvishchahwala@users.noreply.github.com>
2020-08-20 00:32:28 -0500

    Update ComplexNumber.java
    Added Program Description.

--- Task6/ComplexNumber.java
+++ Task6/ComplexNumber.java
@@ -1,3 +1,7 @@
+/**
+* Print the sum, difference and product of two complex numbers by creating a class named 'Complex' with separate methods for each operation 
+* whose real and imaginary parts are entered by user.
+*/
 import java.util.Scanner;
 
 public class ComplexNumber {
