fe588d3a8dc9f92f2b9af8e93580db651ec9fb9d
Urvish Chahwala <35097100+urvishchahwala@users.noreply.github.com>
2020-08-20 00:36:58 -0500

    Update Programing.java
    Added Description of Program.

--- Task6/Programing.java
+++ Task6/Programing.java
@@ -1,3 +1,7 @@
+/**
+* Create a class named 'Programming'. While creating an object of the class, if nothing is passed to it, then the message "I love programming languages" should be printed. 
+* If some String is passed to it, then in place of "programming languages" the name of that String variable should be printed. 
+*/
 public class Programing {
 
 	Programing() {
