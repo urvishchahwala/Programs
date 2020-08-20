8e66361f1b17942dc9d72c0ca239116e5be4e372
Urvish Chahwala <35097100+urvishchahwala@users.noreply.github.com>
2020-08-20 00:35:03 -0500

    Update Employee.java
    Added Program Description.

--- Task6/Employee.java
+++ Task6/Employee.java
@@ -1,3 +1,15 @@
+/**
+* Write a program that would print the information (name, year of joining, salary, address) of three employees by creating a class named 'Employee'. 
+* The output should be as follows: 
+
+*  Name     Year of joining      Address 
+
+*  Robert    1994               64C- Walls Streat 
+
+*  Sam       2000               68D- Walls Streat 
+
+*  John      1999               26B- Walls Streat 
+*/
 public class Employee {
 
 	String name, address;
