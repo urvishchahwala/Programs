ba3f834a0dd2b883f1c28ec6c78882c00b59891f
Urvish Chahwala <35097100+urvishchahwala@users.noreply.github.com>
2020-08-20 00:13:55 -0500

    Update Isogram.java

--- Task5/Isogram.java
+++ Task5/Isogram.java
@@ -1,3 +1,7 @@
+/**
+*  Write a JAVA method to return a boolean true if the string doesn’t have repeating letters and consecutive or nonconsecutive
+*  i.e.all the letters of the string be unique (isogram) else false. 
+*/
 public class Isogram {
 
 	public static void main(String args[]) {
